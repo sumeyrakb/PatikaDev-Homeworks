8807cbd658511959cc9bb0e8a5129e5ce3dac2d0
Sumeyra Yesim Karabocek <95500668+sumeyrakb@users.noreply.github.com>
2022-08-01 14:24:05 +0300

    commit

---
+++ src/Homework/RecursiveUsAlma.java
@@ -0,0 +1,30 @@
+package Homework;
+
+import java.util.Scanner;
+
+public class RecursiveUsAlma {
+
+    static int result=1;
+    public static int power(int exp, int base) {
+
+        if (exp == 0)
+            return 1;
+        result *= base;
+        power(exp-1,base);
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int exponent, base;
+
+        System.out.print("Taban Degeri Giriniz : ");
+        base = scan.nextInt();
+
+        System.out.print("Us Degerini giriniz : ");
+        exponent = scan.nextInt();
+
+        System.out.println("Sonuc : "+power(exponent,base));
+
+    }
+}
